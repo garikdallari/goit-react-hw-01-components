0fbec21878d0db8d47204b2095175e45adf188c2
garikdallari <garikdallari@gmail.com>
2021-08-26 18:01:20 +0300

    Updates

--- static/js/main.ef7f10d1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{2:function(t,n,o){"use strict";o.r(n);o(0),o(4),o(8),o(9)},8:function(t,n,o){},9:function(t,n){}},[[2,1,2]]]);
-//# sourceMappingURL=main.ef7f10d1.chunk.js.map
